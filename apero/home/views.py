357115779ce580396e5e89a0c13f109783a7038a
Antoine Reversat <areversat@caravan.coop>
2014-09-15 22:24:33 -0400

    Use the new ActiveTabMixin in our view

--- apero/home/views.py
+++ apero/home/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView
+from .mixins import ActiveTabMixin
 
-class HomeView(TemplateView):
+class HomeView(ActiveTabMixin, TemplateView):
 
     template_name = "home.html"
-
+    active_tab = "home"
 
